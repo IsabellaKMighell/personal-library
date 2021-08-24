9f30976a5e12fe497346deaa434d748c48205111
GiuseppePennisi <jojop101@gmail.com>
2021-08-24 19:56:11 -0400

    updated event listeners and HTML

--- library.js
+++ library.js
@@ -1,45 +1,40 @@
- 
 const form = document.querySelector("#book-form")
 form.addEventListener("submit", e=>{
     e.preventDefault();
     const userInput = e.target[0].value
     console.log(userInput)
     form.reset();
-})
-
-const libraryButton = document.querySelector("#add-to-library");
-libraryButton.addEventListener('click', e=>{
-    libraryButton.innerText = "✓"
-})
-
-const favoriteButton = document.querySelector("#favorite")
-favoriteButton.addEventListener('click', e=>{
-    favoriteButton.innerText = "❤"
-})
-
-//export
-import { yourAPIKey } from './config.js';
-const title ='john adams'
-
-
-
-
-const info = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}&key=${yourAPIKey}`
-fetch(info)
-.then(resp => resp.json())
-.then(data => {
+    const info = `https://www.googleapis.com/books/v1/volumes?q=intitle:${userInput}&key=${yourAPIKey}`
+    fetch(info)
+    .then(resp => resp.json())
+    .then(data => {
     //console.log(data)
    console.log(data.items)
    renderBooks(data.items)
     })
-
+})
+// const libraryButton = document.querySelector("#add-to-library");
+// libraryButton.addEventListener('click', e=>{
+//     libraryButton.innerText = "✓"
+//     favoriteDiv = document.querySelector("#favorite-books > div")
+//     const bookTitle = document.querySelector("#detailed-info > p:nth-child(2) > span")
+//     favoriteDiv.innerHTML = `<img src="queryselector" /> <div>queryselectTitle</div>`
+// })
+// const favoriteButton = document.querySelector("#favorite")
+// favoriteButton.addEventListener('click', e=>{
+//     favoriteButton.innerText = ":heart:"
+// })
+//export
+import { yourAPIKey } from './config.js';
+const title ='john adams'
 function renderBooks(books){
     books.forEach(element => {
-        // console.log(element)
-        // console.log(element.volumeInfo.title)
-        // console.log(element.volumeInfo.authors)
-        // console.log(element.volumeInfo.description)
-        // console.log(element.volumeInfo.imageLinks.thumbnail)
+        const bookTitle = document.querySelector("#detailed-info > p:nth-child(2) > span")
+        bookTitle.innerText = element.volumeInfo.title
+        const bookImage = document.querySelector("#detailed-info > img")
+        bookImage.src = element.volumeInfo.imageLinks.thumbnail
+        const bookDes = document.querySelector("#detailed-info > p:nth-child(3)")
+        bookDes.innerText = element.volumeInfo.description
         const book = {
             title: element.volumeInfo.title,
             authors: element.volumeInfo.authors,
@@ -47,6 +42,23 @@ function renderBooks(books){
             imageLinks: element.volumeInfo.imageLinks.thumbnail
         }
         console.log(book)
+        const libraryButton = document.querySelector("#add-to-library");
+        libraryButton.addEventListener('click', e=>{
+        libraryButton.innerText = "✓"
+        // const addBooks = document.querySelector("#to-read")
+        const libraryDiv = document.querySelector("#want-to-read")
+        const newLibraryDiv = document.createElement('div')
+        newLibraryDiv.innerHTML = `<img src=${book.imageLinks} /> <div>${book.title}</div>`
+        // addBooks.append(libraryDiv)
+        libraryDiv.appendChild(newLibraryDiv)
     });
+        const favoriteButton = document.querySelector("#favorite")
+        favoriteButton.addEventListener('click', e=>{
+        favoriteButton.innerText = ":heart:"
+        const favoriteDiv = document.querySelector("#favorite-books > div")
+        // const
+        favoriteDiv.innerHTML = `<img src=${book.imageLinks} /> <div>${book.title}</div>`
+        addBooks.append(favoriteDiv)
+})
+    })
 }
-console.log(info)
