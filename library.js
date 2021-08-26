60032baf006208c88096e175aaad064d344669aa
chaim <chaimyisroel@gmail.com>
2021-08-25 22:51:33 -0400

    updated alert

--- library.js
+++ library.js
@@ -1,184 +1,167 @@
-import { yourAPIKey } from './config.js';
-const title ='john adams'
-const bookIdListRead = []
-const bookIdListFave = []
+import { yourAPIKey } from "./config.js";
+const title = "john adams";
+const bookIdListRead = [];
+const bookIdListFave = [];
 
-const form = document.querySelector("#book-form")
-form.addEventListener("submit", e=>{
+const form = document.querySelector("#book-form");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const userInput = e.target[0].value
+    const userInput = e.target[0].value;
     //console.log(userInput)
-    document.querySelector('.book-display').innerHTML = ''
+    document.querySelector(".book-display").innerHTML = "";
     form.reset();
-    
-    const info = `https://www.googleapis.com/books/v1/volumes?q=intitle:${userInput}&key=${yourAPIKey}`
+
+    const info = `https://www.googleapis.com/books/v1/volumes?q=intitle:${userInput}&key=${yourAPIKey}`;
     fetch(info)
-    .then(resp => resp.json())
-    .then(data => {
-    //console.log(data)
-   //console.log(data.items)
-   renderBooks(data.items)
-    })
-})
+        .then((resp) => resp.json())
+        .then((data) => {
+            //console.log(data)
+            //console.log(data.items)
+            renderBooks(data.items);
+        });
+});
 
-
-
-function renderBooks(books){
-    books.forEach(element => {
+function renderBooks(books) {
+    books.forEach((element) => {
         //console.log(element)
-         const book = {
+        const book = {
             title: element.volumeInfo.title,
             authors: element.volumeInfo.authors,
             description: element.volumeInfo.description,
             imageLinks: element.volumeInfo.imageLinks.thumbnail,
-            bookID: element.id
-        }
-        console.log(book)
-        const bookList = document.querySelector('.book-display')
+            bookID: element.id,
+        };
+        console.log(book);
+        const bookList = document.querySelector(".book-display");
         //console.log(bookList)
-        const detailedInfo = document.createElement('div')
-        detailedInfo.id = 'detailed-info'
+        const detailedInfo = document.createElement("div");
+        detailedInfo.id = "detailed-info";
 
-        
         //image element
-        const img = document.createElement('img')
-        img.src = book.imageLinks
+        const img = document.createElement("img");
+        img.src = book.imageLinks;
 
         //buttons and book title
-        const buttonsAndTitle = document.createElement('div')
+        const buttonsAndTitle = document.createElement("div");
 
-        const libraryButton = document.createElement('button')
-        libraryButton.innerText = "▻"
-        libraryButton.id = "add-to-library"
+        const libraryButton = document.createElement("button");
+        libraryButton.innerText = "▻";
+        libraryButton.id = "add-to-library";
 
-        const bookTitleElement = document.createElement('span')
-        bookTitleElement.innerText = book.title
+        const bookTitleElement = document.createElement("span");
+        bookTitleElement.innerText = book.title;
 
-        const faveButton = document.createElement('button')
-        faveButton.innerText = '♡'
-        faveButton.id = "favorite"
+        const faveButton = document.createElement("button");
+        faveButton.innerText = "♡";
+        faveButton.id = "favorite";
 
-        buttonsAndTitle.append(libraryButton, bookTitleElement, faveButton)
-        
+        buttonsAndTitle.append(libraryButton, bookTitleElement, faveButton);
 
-        const imgTitleDiv = document.createElement('div')
-        imgTitleDiv.append(img, buttonsAndTitle)
-        
-        
-
+        const imgTitleDiv = document.createElement("div");
+        imgTitleDiv.append(img, buttonsAndTitle);
 
         //book description
-        const bookDescription = document.createElement('div')
-        bookDescription.innerText = book.description
-        
-    
+        const bookDescription = document.createElement("div");
+        bookDescription.innerText = book.description;
 
-        detailedInfo.append(imgTitleDiv ,bookDescription)
-        detailedInfo.style.display = "flex"
-        detailedInfo.style.padding = '10px'
+        detailedInfo.append(imgTitleDiv, bookDescription);
+        detailedInfo.style.display = "flex";
+        detailedInfo.style.padding = "10px";
 
-        bookList.appendChild(detailedInfo)
+        bookList.appendChild(detailedInfo);
 
-
-        libraryButton.addEventListener('click',  e=>{
-            const bookisinlist = bookIdListRead.includes(book.bookID)
+        libraryButton.addEventListener("click", (e) => {
+            const bookisinlist = bookIdListRead.includes(book.bookID);
             if (bookisinlist === true) {
-                alert('This is a Double Read')
+                alert(`${book.title} is already on the Books to Read list`);
             } else {
-                bookIdListRead.push(book.bookID)
-                libraryButton.innerText='✓'
+                bookIdListRead.push(book.bookID);
+                libraryButton.innerText = "✓";
 
-                addToRead(book)
+                addToRead(book);
 
                 fetch("http://localhost:3000/library", {
-                method: "POST",
-                headers:{
-                "Content-Type":"application/json"
-                },
-                body:JSON.stringify(book)
-                })
-                
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(book),
+                });
             }
+        });
 
-        })
-       
-
-        faveButton.addEventListener('click', e=>{
-            const bookisinlistFave = bookIdListFave.includes(book.bookID)
+        faveButton.addEventListener("click", (e) => {
+            const bookisinlistFave = bookIdListFave.includes(book.bookID);
             if (bookisinlistFave === true) {
-                alert('This is a Double Fave')
+                alert(`${book.title} is already on the Favorite Book list`);
             } else {
-                bookIdListFave.push(book.bookID)
-                faveButton.innerText = '❤'
+                bookIdListFave.push(book.bookID);
+                faveButton.innerText = "❤";
 
-                addToFavorite(book)
+                addToFavorite(book);
 
                 fetch("http://localhost:3000/favorites", {
-                method: "POST",
-                headers:{
-                "Content-Type":"application/json"
-                },
-                body:JSON.stringify(book)
-                })
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(book),
+                });
             }
-        
-        })
-
-    })
+        });
+    });
 }
 
-
-
 fetch("http://localhost:3000/library")
-    .then(resp => resp.json())
-    .then(books => books.forEach(book=> {
-        bookIdListRead.push(book.bookID)
-        addToRead(book)
-    }))
+    .then((resp) => resp.json())
+    .then((books) =>
+        books.forEach((book) => {
+            bookIdListRead.push(book.bookID);
+            addToRead(book);
+        })
+    );
 
-function addToRead(book){
+function addToRead(book) {
     //console.log(`clicked addToLibrary     ${book.title}`)
-    const wantToReadFlexContainer = document.querySelector("#to-read")
-    const toRead = document.createElement('div')
-    toRead.className = 'wants'
+    const wantToReadFlexContainer = document.querySelector("#to-read");
+    const toRead = document.createElement("div");
+    toRead.className = "wants";
 
-    const img = document.createElement('img')
-    img.src = book.imageLinks
-    const title = document.createElement('div')
-    title.innerText = book.title
+    const img = document.createElement("img");
+    img.src = book.imageLinks;
+    const title = document.createElement("div");
+    title.innerText = book.title;
 
-    toRead.append(img,title)
-    toRead.style.padding = "5px"
+    toRead.append(img, title);
+    toRead.style.padding = "5px";
 
-    wantToReadFlexContainer.appendChild(toRead)
-    wantToReadFlexContainer.style.display = "flex"
-
-
-
+    wantToReadFlexContainer.appendChild(toRead);
+    wantToReadFlexContainer.style.display = "flex";
 }
 
 fetch("http://localhost:3000/favorites")
-    .then(resp => resp.json())
-    .then(books => books.forEach(book=> {
-        bookIdListFave.push(book.bookID)
-        addToFavorite(book)
-    }))
+    .then((resp) => resp.json())
+    .then((books) =>
+        books.forEach((book) => {
+            bookIdListFave.push(book.bookID);
+            addToFavorite(book);
+        })
+    );
 
-function addToFavorite(book){
+function addToFavorite(book) {
     //console.log(`clicked favorite     ${book.title}`)
-    const favoritesFlexContainer = document.querySelector("#faves")
-    const faves = document.createElement('div')
-    faves.className = "favorites"
-    
-    const img = document.createElement('img')
-    img.src = book.imageLinks
-    const title = document.createElement('div')
-    title.innerText = book.title
+    const favoritesFlexContainer = document.querySelector("#faves");
+    const faves = document.createElement("div");
+    faves.className = "favorites";
 
-    faves.append(img,title)
-    faves.style.padding = "5px"
+    const img = document.createElement("img");
+    img.src = book.imageLinks;
+    const title = document.createElement("div");
+    title.innerText = book.title;
 
-    favoritesFlexContainer.appendChild(faves)
-    favoritesFlexContainer.style.display="flex"
+    faves.append(img, title);
+    faves.style.padding = "5px";
+
+    favoritesFlexContainer.appendChild(faves);
+    favoritesFlexContainer.style.display = "flex";
 }
-
